8706a1ec360eef044a5870cb2855218f3f88d149
DoomedPerson <37386791+DoomedPerson@users.noreply.github.com>
2023-09-07 19:20:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -93,3 +93,70 @@ function createTab(tabName) {
     closeButton.appendChild(img);
 }
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const editor = document.getElementById('readmeTextbox');
+    const lineCount = document.getElementById('linecount');
+
+    editor.addEventListener('click', () => {
+        highlightCurrentLine("click");
+    });
+
+    editor.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' || e.key === 'Backspace' || e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+            setTimeout(() => {
+                highlightCurrentLine(e);
+            }, 10);
+        }
+    });
+
+    function highlightCurrentLine(e) {
+        const selection = window.getSelection();
+        const range = selection.getRangeAt(0);
+
+        // Get the startContainer (where the selection or caret is)
+        const startContainer = range.startContainer;
+
+        // Check if the startContainer is the editor itself
+        if (startContainer === editor) {
+            // If the caret is at the beginning of the editor, set currentLine to the first line
+            const currentLine = editor.querySelector('.line');
+            
+            // Remove previous highlight from all lines
+            const highlightedLines = document.querySelectorAll('.line-highlight');
+            highlightedLines.forEach((line) => {
+                line.classList.remove('line-highlight');
+            });
+
+            // Add highlight to the current line
+            currentLine.classList.add('line-highlight');
+        } else {
+            
+            const currentLine = startContainer.nodeType === Node.TEXT_NODE ? startContainer.parentElement : startContainer;
+
+            // Remove previous highlight from all lines
+            const highlightedLines = document.querySelectorAll('.line-highlight');
+            highlightedLines.forEach((line) => {
+                line.classList.remove('line-highlight');
+            });
+
+            
+            currentLine.classList.add('line-highlight');
+        }
+        // Deal with line numbers
+        const numb = editor.childNodes.length;
+
+        if ((numb !== lineCount.childElementCount) || (numb === 1 && e.key === 'Enter')) {
+            if ((lineCount.childElementCount > 1 && e.key !== 'Enter') || (lineCount.childElementCount < numb)) {
+                lineCount.innerHTML = "";
+            }
+
+            for (let i = 1; i <= numb; i++) {
+                const line = document.createElement("div");
+                line.id = "line";
+                line.textContent = String(i);
+                lineCount.appendChild(line);
+            }
+        }
+    }
+});
